7e4c71f110cff57535dadf0f5ba77ee52af56474
Tomer Sedan <tomer.sedan@gmail.com>
2019-01-15 17:51:16 -0800

    Updates

--- precache-manifest.b14a2db54fa47f20068f68f08c13a843.js
+++ precache-manifest.b14a2db54fa47f20068f68f08c13a843.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "a91e8e70a4483213a5ca",
-    "url": "/static/js/main.a91e8e70.chunk.js"
+    "revision": "30273066a88d885940a1",
+    "url": "/static/js/main.30273066.chunk.js"
   },
   {
     "revision": "cae40d40925b4cad1c4b",
     "url": "/static/js/1.cae40d40.chunk.js"
   },
   {
-    "revision": "a91e8e70a4483213a5ca",
-    "url": "/static/css/main.c7e7d2f3.chunk.css"
+    "revision": "30273066a88d885940a1",
+    "url": "/static/css/main.e061cb61.chunk.css"
   },
   {
     "revision": "cae40d40925b4cad1c4b",
     "url": "/static/css/1.e7c13443.chunk.css"
   },
   {
-    "revision": "2f03665cf4487f5ad1591bc6a81d59f9",
+    "revision": "01e7fb2b5cf8370365258aec94d4c125",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7dd1e47e12ffe351139d8b9415c4661e.js"
+  "/precache-manifest.b14a2db54fa47f20068f68f08c13a843.js"
 );
 
 workbox.clientsClaim();
