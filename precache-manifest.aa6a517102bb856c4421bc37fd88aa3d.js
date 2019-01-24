2765753a0f0c78715710577cd1854cf8d7dab254
Tomer Sedan <tomer.sedan@gmail.com>
2019-01-24 14:54:31 -0800

    Updates

---
+++ precache-manifest.aa6a517102bb856c4421bc37fd88aa3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3e420f57e091f8e0737",
+    "url": "/nugwa/static/js/runtime~main.b3e420f5.js"
+  },
+  {
+    "revision": "989bf1731ce2e578e8e9",
+    "url": "/nugwa/static/js/main.989bf173.chunk.js"
+  },
+  {
+    "revision": "cae40d40925b4cad1c4b",
+    "url": "/nugwa/static/js/1.cae40d40.chunk.js"
+  },
+  {
+    "revision": "989bf1731ce2e578e8e9",
+    "url": "/nugwa/static/css/main.79696a35.chunk.css"
+  },
+  {
+    "revision": "cae40d40925b4cad1c4b",
+    "url": "/nugwa/static/css/1.e7c13443.chunk.css"
+  },
+  {
+    "revision": "f0fa46d6d49c38940af1d0441a9ffffe",
+    "url": "/nugwa/index.html"
+  }
+];
