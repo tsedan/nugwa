a260bb4197f3c7ba0c27a228ecb26e85537ad6b6
Tomer Sedan <tomer.sedan@gmail.com>
2019-01-24 14:59:22 -0800

    Updates

---
+++ precache-manifest.df7329468c3fd9e54dec3499cced2e63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "989bf1731ce2e578e8e9",
+    "url": "/static/js/main.989bf173.chunk.js"
+  },
+  {
+    "revision": "cae40d40925b4cad1c4b",
+    "url": "/static/js/1.cae40d40.chunk.js"
+  },
+  {
+    "revision": "989bf1731ce2e578e8e9",
+    "url": "/static/css/main.79696a35.chunk.css"
+  },
+  {
+    "revision": "cae40d40925b4cad1c4b",
+    "url": "/static/css/1.e7c13443.chunk.css"
+  },
+  {
+    "revision": "814a66b7a2d2184bb504d5c670681a41",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/nugwa/precache-manifest.aa6a517102bb856c4421bc37fd88aa3d.js"
+  "/precache-manifest.df7329468c3fd9e54dec3499cced2e63.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/nugwa/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
