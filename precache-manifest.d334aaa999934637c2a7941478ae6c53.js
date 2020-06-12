9fd618d27fc0bd35894c09a5213e9fc1c2e2f370
Tomer <tomer.sedan@protonmail.com>
2020-06-12 15:51:56 -0700

    Updates

---
+++ precache-manifest.d334aaa999934637c2a7941478ae6c53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b42eb34cc4018e20a499",
+    "url": "/nugwa/static/css/main.37ea6348.chunk.css"
+  },
+  {
+    "revision": "b42eb34cc4018e20a499",
+    "url": "/nugwa/static/js/main.724e70cc.chunk.js"
+  },
+  {
+    "revision": "9c89339d07937eb2237c",
+    "url": "/nugwa/static/js/runtime~main.b99e8200.js"
+  },
+  {
+    "revision": "e19145a15dcc2b50f871",
+    "url": "/nugwa/static/css/2.7f85df58.chunk.css"
+  },
+  {
+    "revision": "e19145a15dcc2b50f871",
+    "url": "/nugwa/static/js/2.8b637f46.chunk.js"
+  },
+  {
+    "revision": "86a8ff5c9255aad1ef5acd3efce00e8a",
+    "url": "/nugwa/static/media/NoSchool.86a8ff5c.gif"
+  },
+  {
+    "revision": "24af1a8d933a8251d09f01aae49b44c4",
+    "url": "/nugwa/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.575c103f8a2461fd309e05224d2c7efd.js"
+  "/nugwa/precache-manifest.d334aaa999934637c2a7941478ae6c53.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/nugwa/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
